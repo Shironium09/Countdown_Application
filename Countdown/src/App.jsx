6cf6cb7ce70955ca109a908a86519f29a417614b
Dustin.JB <dustinj.bal09@gmail.com>
2025-11-12 13:43:20 +0800

    Basic UI done

--- Countdown/src/App.jsx
+++ Countdown/src/App.jsx
@@ -1,14 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // const timer = [currentTime, setCurrentTime] = useState(new Date());
 
   return (
     <>
-      
+      <div className="main_container">
+        <div className="countdown_container">
+            <div className="hour_container timer_container">
+              <h1>00</h1>
+              <h3>Hours</h3>
+            </div>
+            <h1>:</h1>
+            <div className="minute_container timer_container">
+              <h1>00</h1>
+              <h3>Minutes</h3>
+            </div>
+            <h1>:</h1>
+            <div className="second_container timer_container">
+              <h1>00</h1>
+              <h3>Seconds</h3>
+            </div>
+        </div>
+      </div>
     </>
   )
 }
